77a56bad1946082c5f2306a7901068d084e2cc12
loopid <intp@dutchmail.com>
2021-08-21 11:32:39 -0400

    added comments

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,5 +1,5 @@
 function getword(info, tab) {
-    fetch('http://wordsmith.qc24.xyz/?word=death')
+    fetch('http://wordsmith.qc24.xyz/?word=death') // ask lukas how this works
         .then(response => response.json())
         .then(data => console.log(data));
     console.log("Word " + info.selectionText + " was clicked.");
@@ -7,6 +7,7 @@ function getword(info, tab) {
         url: "http://wordsmith.qc24.xyz/?word=" + info.selectionText
     });
 }
+// this block updates the menu u get when u right click on chrome
 chrome.contextMenus.create({
     title: "Search: %s",
     contexts: ["selection"],
