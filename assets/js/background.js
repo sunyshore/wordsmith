31b3793cfce2825412c65b38140211cfbb1b2260
loopid <intp@dutchmail.com>
2021-08-21 02:19:27 -0400

    testing popups

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,1 +1,11 @@
-chrome.runtime.onInstalled.addListener(function() { });
+function getword(info,tab) {
+    console.log("Word " + info.selectionText + " was clicked.");
+    chrome.tabs.create({  // shouldn't make a new tab
+      url: "http://wordsmith.qc24.xyz/?word=" + info.selectionText
+    });
+  }
+  chrome.contextMenus.create({
+    title: "Search: %s", 
+    contexts:["selection"], 
+    onclick: getword
+  });
